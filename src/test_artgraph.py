fa41230386128c7f902e46a34b4421fcc028831b
faiail <raffaele.scaringi20@gmail.com>
2023-02-17 10:32:28 +0100

    script for test

--- src/test_artgraph.py
+++ src/test_artgraph.py
@@ -6,11 +6,6 @@ import os
 from tqdm import tqdm
 
 
-"""
-TRY TO OPTIMIZE WITH PARALLELISM
-"""
-
-
 def get_data(path: str = 'data.csv', manager = None):
     if os.path.exists(path):
         return pd.read_csv(path, index_col=0)
@@ -20,21 +15,23 @@ def get_data(path: str = 'data.csv', manager = None):
     return data
 
 
+def query(payload, url, headers):
+    response = requests.post(url, headers=headers, json={"inputs": payload})
+    return response.json()
+
 if __name__ == '__main__':
     tqdm.pandas()
     key = json.load(open('key.json'))['key']
     API_URL = "https://api-inference.huggingface.co/models/beyond/genius-large-k2t"
     headers = {"Authorization": f"Bearer {key}"}
 
-    def query(payload):
-        response = requests.post(API_URL, headers=headers, json={"inputs": payload})
-        return response.json()
-
     manager = DataManager()
     data = get_data('data.csv', manager)
 
-    tqdm.pandas()
-    print('start')
-    data['prompt'] = data.progress_apply(lambda x: manager.get_template_by_artwork(x['name']), axis=1)
-    #data['caption'] = data.iloc[:50_000].progress_apply(lambda x: query(manager.get_prompt_by_artwork(x['name'])), axis=1)
+    if 'prompt' not in data.columns:
+        data['prompt'] = data.progress_apply(lambda x: manager.get_prompt_by_artwork(x['name']), axis=1)
+
+    if 'caption' not in data.columns:
+        data['caption'] = data.progress_apply(lambda x: query(x['prompt'], API_URL, headers), axis=1)
+
     data.to_csv('data.csv')
